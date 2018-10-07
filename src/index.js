79c29cf42bb6d815ce2359ee191e176df1154e88
Konstantin Savransky <ksavransky@gmail.com>
2018-10-06 17:47:11 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { HttpLink, createHttpLink } from "apollo-link-http";
+
+const hideMeToken = '?token=HWm0tPtem7oY2xJ8MBhPn7RpTTpXCaZF'
+
+const client = new ApolloClient({
+  uri: "http://api.kittyhawk.io/graphql" + hideMeToken,
+  fetchOptions: { method: "post" }
+});
+
+
+
+
+client
+  .query({
+    query: gql`
+      {
+         account {
+           users {
+             id
+           }
+         }
+       }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
